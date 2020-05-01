d31a216f6df168df590bcf069c684fe8e9d123e7
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-05-01 20:52:52 +0530

    changes made to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
 
+cwd = os.getcwd()
 output_model_path = os.path.join(cwd,"model.pkl")
 
 #testing_data_path = "E://Study//GitLab//SMS_Classification//data//testing_data.csv"
