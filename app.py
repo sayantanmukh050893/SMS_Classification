d3e48b0cffac0fc3a9850bc83150fcbc86bb31c8
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-05-01 20:51:34 +0530

    app.py uploaded

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask,request,jsonify,render_template
 import pickle
 from util import Util
 util = Util()
+import os
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
-import nltk
 
-output_model_path = "E://Study//GitLab//SMS_Classification//src//model.pkl"
-testing_data_path = "E://Study//GitLab//SMS_Classification//data//testing_data.csv"
-text_vectorizer_path = "E://Study//GitLab//SMS_Classification//src//text_vectorizer_path.pkl"
+output_model_path = os.path.join(cwd,"model.pkl")
+
+#testing_data_path = "E://Study//GitLab//SMS_Classification//data//testing_data.csv"
+text_vectorizer_path = os.path.join(cwd,"text_vectorizer_path.pkl")
 
 app = Flask(__name__)
 
